e391e48a9cab67077045a5c24af2441f7ee82bd6
Omurzak <omurzak.keldibek@gmail.com>
2023-04-10 00:45:24 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './styles/style.css'
-import MainPage from './components/MainPage'
-import SearchPage from './components/SearchPage'
-import SearchTool from './components/search-tool-features/SearchTool'
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-    return (
-        <BrowserRouter>
-            <main>
-                <SearchTool/>
-                <Routes>
-                    <Route exact path="/" element={<MainPage/>}/>
-                    <Route exact path="/search" element={<SearchPage/>}/>
-                </Routes>
-            </main>
-        </BrowserRouter>
-    )
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
